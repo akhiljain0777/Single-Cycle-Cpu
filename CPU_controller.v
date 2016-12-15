2afb9cd642d8b07296682554660aa71d244ce178
Akhil Jain <akhil.jain077@iitkgp.ac.in>
2016-12-15 22:18:06 +0530

    Update and rename CPU_contorller.v to CPU_controller.v

--- CPU_controller.v
+++ CPU_controller.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:09:28 10/22/2016 
-// Design Name: 
-// Module Name:    CPU_controller 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module CPU_controller(
    C,V,S,Z_det,clk,reset,
 	opc,opd1,opd2,opd3,
